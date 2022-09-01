8bbcaceba8d40268745cb973414e505baccf7312
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-09-01 16:46:19 -0500

    random play list

--- for loop/app.js
+++ for loop/app.js
@@ -1,5 +1,5 @@
 let pearlJamDrummers = ['Dave Krusen', 'Matt Chamberlain', 'Dave Abbruzzese', 'Jack Irons', 'Matt Cameron']
 
 for(let i = 0; i < pearlJamDrummers.length; i++) {
-  console.log(pearlJamDrummers[i]);
+  console.log(pearlJamDrummers[i].toUpperCase());
 }
